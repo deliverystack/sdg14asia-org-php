f34b1ed9e0cc58787cff8d073bca5ac7d61161fb
commodore73 <github@deliverystack.net>
2026-05-25 14:48:25 +0700

    fix include reference

--- public_html/projects.php
+++ public_html/projects.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../../includes/functions.php'; 
+require_once __DIR__ . '/../includes/functions.php';
 
 $metaJSON = '{
     "title": "Marine Kindergarten + Blue Carbon + Plankton Observatory",
